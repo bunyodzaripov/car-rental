c7f5ad5e27286d813c3d50f3b14acced438d916c
Bunyod <zaripovbunyod571@gmail.com>
2026-02-03 03:19:07 +0500

    added header section

--- src/modules/header/index.jsx
+++ src/modules/header/index.jsx
@@ -1,5 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMoon,
+  faChevronDown,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 import { Logo } from "../../assets/images";
 
 const Header = () => {
@@ -7,37 +11,53 @@ const Header = () => {
     {
       label: "Categories",
       dropdown: null,
+      hasIcon: false,
     },
     {
       label: "About Us",
       dropdown: ["News", "Careers", "Locations"],
+      hasIcon: true,
     },
     {
       label: "Help center",
       dropdown: ["FAQ", "Contact us"],
+      hasIcon: true,
     },
   ];
 
   return (
     <header className="w-full shadow-sm">
-      <div className="containers py-6.75 flex items-center justify-between">
+      <div className="containers py-6.75 flex items-center justify-between font-medium text-black text-[18px]">
         <div className="flex gap-9 ">
           <h1>
             <a href="#">
               <img src={Logo} alt="Car Rental Logo" className="w-37.5" />
             </a>
           </h1>
-          <nav className="flex items-center gap-8 text-black text-[18px] font-medium">
+
+          <nav className="flex items-center gap-3">
             {navList.map((item, index) => (
               <div key={index} className="relative group">
-                <li className="flex cursor-pointer gap-1 text">{item.label}</li>
+                <li className="flex items-center gap-2 cursor-pointer px-5 py-3 rounded-full transition-all duration-300 group-hover:bg-[#F5F5F5]">
+                  <span>{item.label}</span>
+
+                  {item.hasIcon && (
+                    <FontAwesomeIcon
+                      icon={faChevronDown}
+                      className="transition-transform duration-300 group-hover:rotate-180"
+                    />
+                  )}
+                </li>
 
                 {item.dropdown && (
-                  <div className="absolute top-full left-0 mt-4 bg-white shadow-xl rounded-2xl p-6 w-56 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200">
-                    <ul className="space-y-3 ">
-                      {item.dropdown.map((subItem, i) => (
-                        <li key={i} className="cursor-pointer">
-                          {subItem}
+                  <div className="absolute left-0 mt-2.5 bg-white shadow-xl rounded-[20px] p-7 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
+                    <ul className="flex flex-col gap-3 whitespace-nowrap">
+                      {item.dropdown.map((dItem, dIndex) => (
+                        <li
+                          key={dIndex}
+                          className="cursor-pointer px-5 py-3  rounded-full transition-all duration-300 hover:bg-[#F5F5F5]"
+                        >
+                          {dItem}
                         </li>
                       ))}
                     </ul>
@@ -48,19 +68,21 @@ const Header = () => {
           </nav>
         </div>
 
-        <div className="flex items-center gap-4">
-          <div className="items-center gap-1 text-gray-700 font-medium">
-            +1 855 420 0000
-            <FontAwesomeIcon icon={faChevronDown} />
+        <div className="flex items-center gap-6">
+          <div className="font-semibold">+1 855 420 0000</div>
+
+          <div className="flex gap-4">
+            <button className="cursor-pointer w-14.5 h-14.5 group rounded-full bg-[#F5F5F5]">
+              <FontAwesomeIcon
+                icon={faMoon}
+                className="text-[#555555] group-hover:text-black transition duration-300"
+              />
+            </button>
+
+            <button className="cursor-pointer bg-[#299764] hover:bg-[#077743] transition duration-300 text-white px-7.5 py-4.5 rounded-full flex items-center gap-3">
+              Login <FontAwesomeIcon icon={faArrowRightToBracket} />
+            </button>
           </div>
-
-          <button className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center hover:bg-gray-200 transition">
-            $
-          </button>
-
-          <button className="bg-green-600 hover:bg-green-700 transition text-white px-5 py-2 rounded-full flex items-center gap-2">
-            Login
-          </button>
         </div>
       </div>
     </header>
