5e5b51bdfd4901a562a419c403e11a5675a31750
Bunyod <zaripovbunyod571@gmail.com>
2026-02-03 03:37:47 +0500

    added navItem component

---
+++ src/components/navItem.jsx
@@ -0,0 +1,36 @@
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const NavItem = ({ item }) => {
+  return (
+    <li className="relative group">
+      <div className="flex items-center gap-2 cursor-pointer px-5 py-3 rounded-full transition-all duration-300 group-hover:bg-[#F5F5F5]">
+        <span>{item.label}</span>
+
+        {item.hasIcon && (
+          <FontAwesomeIcon
+            icon={faChevronDown}
+            className="transition-transform duration-300 group-hover:rotate-180"
+          />
+        )}
+      </div>
+
+      {item.dropdown && (
+        <div className="absolute left-0 mt-2.5 bg-white shadow-xl rounded-[20px] p-7 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
+          <ul className="flex flex-col gap-3 whitespace-nowrap">
+            {item.dropdown.map((dItem, index) => (
+              <li
+                key={index}
+                className="cursor-pointer px-5 py-3 rounded-full transition-all duration-300 hover:bg-[#F5F5F5]"
+              >
+                {dItem}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </li>
+  );
+};
+
+export default NavItem;
--- src/modules/header/index.jsx
+++ src/modules/header/index.jsx
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMoon,
-  faChevronDown,
   faArrowRightToBracket,
 } from "@fortawesome/free-solid-svg-icons";
 import { Logo } from "../../assets/images";
+import { NavItem } from "../../components";
 
 const Header = () => {
   const navList = [
@@ -35,36 +35,12 @@ const Header = () => {
             </a>
           </h1>
 
-          <nav className="flex items-center gap-3">
-            {navList.map((item, index) => (
-              <div key={index} className="relative group">
-                <li className="flex items-center gap-2 cursor-pointer px-5 py-3 rounded-full transition-all duration-300 group-hover:bg-[#F5F5F5]">
-                  <span>{item.label}</span>
-
-                  {item.hasIcon && (
-                    <FontAwesomeIcon
-                      icon={faChevronDown}
-                      className="transition-transform duration-300 group-hover:rotate-180"
-                    />
-                  )}
-                </li>
-
-                {item.dropdown && (
-                  <div className="absolute left-0 mt-2.5 bg-white shadow-xl rounded-[20px] p-7 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
-                    <ul className="flex flex-col gap-3 whitespace-nowrap">
-                      {item.dropdown.map((dItem, dIndex) => (
-                        <li
-                          key={dIndex}
-                          className="cursor-pointer px-5 py-3  rounded-full transition-all duration-300 hover:bg-[#F5F5F5]"
-                        >
-                          {dItem}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                )}
-              </div>
-            ))}
+          <nav>
+            <ul className="flex items-center gap-3">
+              {navList.map((item, index) => (
+                <NavItem key={index} item={item} />
+              ))}
+            </ul>
           </nav>
         </div>
 
